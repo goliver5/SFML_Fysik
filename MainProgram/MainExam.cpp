745c1e41e6981583f8f834abe93f593246dcfc32
MorranMogge <moggewesterberg@gmail.com>
2022-02-21 19:33:24 +0100

    Gjorde rent "Main"

--- MainProgram/MainExam.cpp
+++ MainProgram/MainExam.cpp
@@ -1,6 +1,4 @@
 #include <SFML/Graphics.hpp>
-#include"Fly.h"
-#include"Tomato.h"
 #include<iostream>
 
 #ifdef _DEBUG
@@ -17,17 +15,12 @@ const int WIDTH = 900;
 const int HEIGHT = 600;
 const int CAP = 50;
 
-void drawFlies(Fly arr[], sf::RenderWindow &window);
-void initiateFlies(Fly arr[]);
-void moveFlies(Fly arr[]);
-void spawnFlies(Fly arr[],int &nrOfFliesSpawned);
-bool checkIfDespawn(Tomato &tomato, Fly arr[], int &nrOf, int &nrOffDespawn);
 void initiateText(sf::Font& font, sf::Text& text);
 
 int main()
 {
 	std::srand((unsigned)time(0));
-	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "DV1594 Muntlig tentamen 3");
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Fysik projekt");
 
 	sf::Clock clock;
 	sf::Time elapsedTimeSinceLastUpdate = sf::Time::Zero;
@@ -42,13 +35,6 @@ int main()
 	int nrOfFliesSpawned = 5;
 	int nrOffDespawn = 0;
 
-	Tomato tomato(HEIGHT, WIDTH);
-
-	Fly flies[CAP];
-	initiateFlies(flies);
-
-	spawnFlies(flies, nrOfFliesSpawned);
-
 	while (window.isOpen())
 	{
 
@@ -60,8 +46,7 @@ int main()
 					window.close();
 				}
 			}
-			if (tomato.getHealth() >= 1)
-			{
+		
 			timePlayed += clock.getElapsedTime();
 			elapsedTimeSinceLastUpdate += clock.restart();
 			while (elapsedTimeSinceLastUpdate > timePerFrame)
@@ -69,30 +54,10 @@ int main()
 
 				elapsedTimeSinceLastUpdate -= timePerFrame;
 
-				tomato.move();
-				moveFlies(flies);
-				if (checkIfDespawn(tomato, flies, nrOfFliesSpawned, nrOffDespawn))
-				{
-					spawnFlies(flies, nrOfFliesSpawned);
-				}
-				//text.setString(std::to_string(tomato.getHealth()));
-				text.setString("Time: " + std::to_string(timePlayed.asSeconds()) + 
-								"\nHealth: " + std::to_string(tomato.getHealth()));
+				text.setString("Time: " + std::to_string(timePlayed.asSeconds()));
 
 			}
-		}
 		window.clear();
-
-		if (tomato.getHealth() >= 1)
-		{
-			drawFlies(flies, window);
-			window.draw(tomato);
-		}
-		else
-		{
-			text.setString("Time: " + std::to_string(timePlayed.asSeconds()) +
-				"\nGame Over");
-		}
 		window.draw(text);
 
 		window.display();
@@ -100,71 +65,6 @@ int main()
 
 
 	return 0;
-}
-
-void drawFlies(Fly arr[], sf::RenderWindow& window)
-{
-	for (int i = 0; i < CAP; i++)
-	{
-		window.draw(arr[i]);
-	}
-}
-
-void initiateFlies(Fly arr[])
-{
-	for (int i = 0; i < CAP; i++)
-	{
-		arr[i].deSpawn();
-		arr[i].setWindow(HEIGHT, WIDTH);
-	}
-}
-
-void moveFlies(Fly arr[])
-{
-	for (int i = 0; i < CAP; i++)
-	{
-		arr[i].move();
-	}
-}
-
-void spawnFlies(Fly arr[], int& nrOfFliesSpawned)
-{
-	for (int i = 0; i < nrOfFliesSpawned; i++)
-	{
-		arr[i].spawn();
-	}
-	//nrOfFliesSpawned += 5;
-}
-
-bool checkIfDespawn(Tomato &tomato, Fly arr[], int &nrOf, int &nrOffDespawn)
-{
-
-	bool allHaveDespawned = false;
-	for (int i = 0; i < nrOf; i++)
-	{
-		if (arr[i].getXposition() + arr[i].getBounds().width < 0.0f)// om utanför fönstret
-		{
-			nrOffDespawn++;
-			arr[i].deSpawn();
-		}
-		if (tomato.isHit(arr[i]))//kod för if hit
-		{
-			arr[i].deSpawn();
-			nrOffDespawn++;
-			tomato.decreaseHealth(arr[i]);
-		}
-		
-	}
-	if (nrOffDespawn == nrOf)
-	{
-		allHaveDespawned = true;
-		nrOffDespawn = 0;
-		if (nrOf < 50)
-		{
-			nrOf += 5;
-		}
-	}
-	return allHaveDespawned;
 }
 
 void initiateText(sf::Font& font, sf::Text& text)
